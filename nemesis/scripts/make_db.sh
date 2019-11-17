f3a268dfaa2a0c0cdf1e0f4bbaeecacafd8ccbd9
Peter Law <PeterJCLaw@gmail.com>
2019-11-17 14:10:23 +0000

    Ensre make_db.sh works regardless of working directory

--- nemesis/scripts/make_db.sh
+++ nemesis/scripts/make_db.sh
@@ -1,24 +1,28 @@
-sqlite3 db/nemesis.sqlite "CREATE table IF NOT EXISTS auth (token VARCHAR(256), username VARCHAR(256))"
-sqlite3 db/nemesis.sqlite "CREATE table IF NOT EXISTS registrations (username         VARCHAR(256) UNIQUE PRIMARY KEY,\
-                                                                     teacher_username VARCHAR(256),\
-                                                                     college          VARCHAR(256),\
-                                                                     team             VARCHAR(256),\
-                                                                     email            VARCHAR(256),\
-                                                                     request_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
-                                                                     verify_code      VARCHAR(256));"
-sqlite3 db/nemesis.sqlite "CREATE table IF NOT EXISTS password_resets (username             VARCHAR(256) UNIQUE PRIMARY KEY,\
-                                                                       requestor_username   VARCHAR(256),\
-                                                                       request_time         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
-                                                                       verify_code          VARCHAR(256));"
-sqlite3 db/nemesis.sqlite "CREATE table IF NOT EXISTS email_changes (username         VARCHAR(256) UNIQUE PRIMARY KEY,\
-                                                                     new_email        VARCHAR(256),\
-                                                                     request_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
-                                                                     verify_code      VARCHAR(256));"
-sqlite3 db/nemesis.sqlite "CREATE table IF NOT EXISTS outbox (id INTEGER PRIMARY KEY ASC AUTOINCREMENT,\
-                                                              toaddr                VARCHAR(256),\
-                                                              template_name         VARCHAR(256),\
-                                                              template_vars_json    VARCHAR(512),\
-                                                              request_time          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                                              last_error            VARCHAR(256),\
-                                                              retry_count           INTEGER DEFAULT 0,\
-                                                              sent_time             TIMESTAMP);"
+#!/bin/sh -e
+
+DB_PATH=$(dirname $0)/../db/nemesis.sqlite
+
+sqlite3 $DB_PATH "CREATE table IF NOT EXISTS auth (token VARCHAR(256), username VARCHAR(256))"
+sqlite3 $DB_PATH "CREATE table IF NOT EXISTS registrations (username         VARCHAR(256) UNIQUE PRIMARY KEY,\
+                                                            teacher_username VARCHAR(256),\
+                                                            college          VARCHAR(256),\
+                                                            team             VARCHAR(256),\
+                                                            email            VARCHAR(256),\
+                                                            request_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
+                                                            verify_code      VARCHAR(256));"
+sqlite3 $DB_PATH "CREATE table IF NOT EXISTS password_resets (username             VARCHAR(256) UNIQUE PRIMARY KEY,\
+                                                              requestor_username   VARCHAR(256),\
+                                                              request_time         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
+                                                              verify_code          VARCHAR(256));"
+sqlite3 $DB_PATH "CREATE table IF NOT EXISTS email_changes (username         VARCHAR(256) UNIQUE PRIMARY KEY,\
+                                                            new_email        VARCHAR(256),\
+                                                            request_time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
+                                                            verify_code      VARCHAR(256));"
+sqlite3 $DB_PATH "CREATE table IF NOT EXISTS outbox (id INTEGER PRIMARY KEY ASC AUTOINCREMENT,\
+                                                     toaddr                VARCHAR(256),\
+                                                     template_name         VARCHAR(256),\
+                                                     template_vars_json    VARCHAR(512),\
+                                                     request_time          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                     last_error            VARCHAR(256),\
+                                                     retry_count           INTEGER DEFAULT 0,\
+                                                     sent_time             TIMESTAMP);"
